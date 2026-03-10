d5221bfd3a46cb31401c70cbbb3799edf2e4c270
Harrison Dano <harrisondano@gmail.com>
2026-03-10 08:40:51 +0800

    fixing

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,18 @@
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
 
-// Same sender ID as in app.js config
 firebase.initializeApp({
-  messagingSenderId: "831045230249"
+  apiKey: "AIzaSyDvAE8hv4ijQ-Lih9cm2xRpf-gFMfWTRnQ",
+  authDomain: "musicteamportalpushapp.firebaseapp.com",
+  projectId: "musicteamportalpushapp",
+  storageBucket: "musicteamportalpushapp.firebasestorage.app",
+  messagingSenderId: "831045230249",
+  appId: "1:831045230249:web:298ed083c299e1b8d413b8"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(payload => {
+messaging.onBackgroundMessage((payload) => {
   console.log("[firebase-messaging-sw.js] Background message:", payload);
 
   const notificationTitle = payload.notification?.title || "New notification";
